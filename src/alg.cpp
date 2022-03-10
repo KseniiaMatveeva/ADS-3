0cd5c9c63830ecb910e6749c3edb36635c688a09
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-03-10 22:49:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,7 +12,7 @@ int cbinsearch(int *arr, int size, int value) {
           count++;
          }
       }
-      if (arr[mid + 1] == value) {
+      while (arr[mid + 1] == value) {
           count++;
           mid++;
         }
